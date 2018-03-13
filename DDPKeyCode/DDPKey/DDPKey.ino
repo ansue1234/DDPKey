2851a0fbdd14ec9f32169b7e4df2d45e7c81f4a2
ansue1234 <ansue1234@gmail.com>
2018-03-12 22:55:05 -0700

    It worked

--- DDPKeyCode/DDPKey/DDPKey.ino
+++ DDPKeyCode/DDPKey/DDPKey.ino
@@ -1,13 +1,15 @@
-const int inputGas = A1;
-const int inputMic = A0;
+const int inputGas = 1;
+const int inputMic = 0;
 const int out = 9;
 const int pwLed = 13;
 const int signalLed = 10;
 
+//if too sensitive 
 const double threGas = 50;
-const double threMic = 50;
+const double threMic = 800;
 
 double gasVal;
+double micVal;
 
 boolean blown;
 
@@ -16,7 +18,7 @@ void setup() {
   pinMode(pwLed, OUTPUT);
   pinMode(signalLed, OUTPUT);
   blown = false;
-  
+  Serial.begin(9600);
   //gather environmental data reference for gas sensor
   initialization();
 }
@@ -24,14 +26,16 @@ void setup() {
 void loop() {
   //show that the code is running
   digitalWrite(pwLed, HIGH);
-  
+  Serial.println(gasVal);
   //Check if human is actually blowing
   start();
   
   //Check alcohol lvl and the corresponding actions
   if(blown){
+    Serial.println(blown);
     delay(500);
-    digitalWrite(signalLed, LOW);
+    digitalWrite(signalLed, HIGH);
+    
     if(alcLvlTest()){
       digitalWrite(out,HIGH);
       delay(6000);
@@ -56,17 +60,20 @@ boolean alcLvlTest(){
 //Gather enviromental references
 void initialization(){
   if(!blown){
+    digitalWrite(pwLed, HIGH);
     delay(1000);
     gasVal = analogRead(inputGas);
     delay(1000);
-    digitalWrite(signalLed, HIGH);
+    //blown = true;
   }
 }
 
 //signal the beginning of check alcohol level by verifing an actual human breathed
 void start(){
-  if(analogRead(inputMic)>threMic){
+  if(analogRead(inputMic)<threMic){
     blown = true;
+  }else{
+    blown = false;
   }
-  blown = false;
 }
+
